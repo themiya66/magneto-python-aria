075124a5351036187135e9fb88d8756166723462
hitesh kamble <77688759+ghostmirrorlab@users.noreply.github.com>
2021-02-24 14:58:57 +0530

    added dead torrent notification message

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -62,7 +62,7 @@ class AriaDownloadHelper(DownloadHelper):
     def __onDownloadStopped(self, api, gid):
         LOGGER.info(f"onDownloadStop: {gid}")
         dl = getDownloadByGid(gid)
-        if dl: dl.getListener().onDownloadError('Download stopped by user!')
+        if dl: dl.getListener().onDownloadError('DEAD TORRENT!')
 
     @new_thread
     def __onDownloadError(self, api, gid):
